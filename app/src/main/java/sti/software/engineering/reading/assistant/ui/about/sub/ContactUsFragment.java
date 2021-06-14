d25d02fd4232dcbcc547907df275ecfb075532d3
Ariel Namias Austria <arielaustria021@gmail.com>
2021-06-15 07:53:34 +0800

    contact us content

--- app/src/main/java/sti/software/engineering/reading/assistant/ui/about/sub/ContactUsFragment.java
+++ app/src/main/java/sti/software/engineering/reading/assistant/ui/about/sub/ContactUsFragment.java
@@ -1,14 +1,19 @@
 package sti.software.engineering.reading.assistant.ui.about.sub;
 
+import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import dagger.android.support.DaggerFragment;
 import sti.software.engineering.reading.assistant.databinding.FragmentContactUsBinding;
+
+import static android.text.Layout.JUSTIFICATION_MODE_INTER_WORD;
 
 public class ContactUsFragment extends DaggerFragment {
 
@@ -19,5 +24,15 @@ public class ContactUsFragment extends DaggerFragment {
                              Bundle savedInstanceState) {
         binding = FragmentContactUsBinding.inflate(inflater);
         return binding.getRoot();
+    }
+
+    @SuppressLint("WrongConstant")
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            binding.tvDescriptionOne.setJustificationMode(JUSTIFICATION_MODE_INTER_WORD);
+            binding.tvDescriptionTwo.setJustificationMode(JUSTIFICATION_MODE_INTER_WORD);
+            binding.tvDescriptionThree.setJustificationMode(JUSTIFICATION_MODE_INTER_WORD);
+        }
     }
 }
